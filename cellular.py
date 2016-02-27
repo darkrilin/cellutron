cc8fe5d77ce8988c4e74be5fa64e2a4c5296feb2
Rilin <theyareinblack@gmail.com>
2016-02-27 14:03:40 +1100

    Activate the Cellutron Initiative

--- cellular.py
+++ cellular.py
@@ -8,19 +8,5 @@ version = '0.0.1'
 def sayhi():
     print("Hello, this is cellular")
 
-def init():
-    pygame.init()
-
-class display():
-    def __init__():
-        pass
-    def create(swidth, sheight):
-        pygame.display.set_mode((swidth, sheight), pygame.DOUBLEBUF)
-    def draw():
-        screen = pygame.display.get_surface()
-        screen.fill((100, 100, 100))
-        pygame.display.flip()
-
-class time():
-    def clock():
+# PUT ALL THE CLASSY CODE HERE
         
