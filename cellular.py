8a639ecc5a7ac08f254607f52192da1e6536a174
Jarred Gallina <jarred.gallina@qxbranch.com>
2016-02-27 14:46:46 +1100

    Classes

--- cellular.py
+++ cellular.py
@@ -8,5 +8,28 @@ version = '0.0.1'
 def sayhi():
     print("Hello, this is cellular")
 
+class Stage():
+    class Agent():
+        def __init__(self, initial_position = (0,0), initial_orientation = 0, costume = None):
+            self.position = initial_orientation
+            self.orientation = initial_orientation
+            self.costume = costume
+
+    class Cell():
+        def __init__(self, colour = (255, 255, 255)):
+            self.colour = colour
+
+    def __init__(self, columns, rows):
+        self.columns = columns
+        self.rows = rows
+        self.cells = {}
+
+        for row in range(self.rows):
+            for column in range(self.colours):
+                self.cells[(row,column)] = self.Cell()
+
+
+
+
 # PUT ALL THE CLASSY CODE HERE
         
