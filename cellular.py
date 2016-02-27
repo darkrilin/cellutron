b6bdfbd1e045a7449d1edc1adcd232d9da4e71d0
Jarred Gallina <jarred.gallina@qxbranch.com>
2016-02-27 16:04:57 +1100

    Added orientation change functions

--- cellular.py
+++ cellular.py
@@ -29,13 +29,34 @@ class Agent():
             self.position = initial_position
         self.orientation = initial_orientation
         self.costume = costume
-        self.orientation_dict = {'N':[0,-1], 'NE':[1,-1,], 'E':[1,0], 'SE':[1,1], 'S':[1,0], 'SW':[1,-1], 'W':[0,-1], 'NW':[-1,-1]}
+        self.ORIENTATION_COORDINATES = {'N':[0,-1], 'NE':[1,-1,], 'E':[1,0], 'SE':[1,1], 'S':[1,0], 'SW':[1,-1], 'W':[0,-1], 'NW':[-1,-1]}
+        self.ORIENTATION_DEGREES = {'N':0, 'NE':45, 'E':90, 'SE':135, 'S':180, 'SW':225, 'W':270, 'NW':315}
+        self.ORIENTATION_DEGREES_REVERSE = {0:'N', 45:'NE', 90:'E', 135:'SE', 180:'S', 225:'SW', 270:'W', 315:'NW'}
         stage.agents.append(self)
 
     def move_steps_forward(self,steps):
-        self.position[0] += self.orientation_dict[self.orientation][0]*steps
-        self.position[1] += self.orientation_dict[self.orientation][0]*steps
+        self.position[0] += self.ORIENTATION_COORDINATES[self.orientation][0]*steps
+        self.position[1] += self.ORIENTATION_COORDINATES[self.orientation][0]*steps
 
+    def rotate_clockwise(self, degrees):
+        assert degrees % 45 == 0; 'Can only turn in 45 degree increments!'
+        angle = self.ORIENTATION_DEGREES[self.orientation]
+        angle += degrees
+        while angle > 360:
+            angle -= 360
+        while angle < 0:
+            angle += 360
+        self.orientation = self.ORIENTATION_DEGREES_REVERSE[angle]
+
+   def rotate_anticlockwise(self, degrees):
+        assert degrees % 45 == 0; 'Can only turn in 45 degree increments!'
+        angle = self.ORIENTATION_DEGREES[self.orientation]
+        angle -= degrees
+        while angle > 360:
+            angle -= 360
+        while angle < 0:
+            angle += 360
+        self.orientation = self.ORIENTATION_DEGREES_REVERSE[angle]
 
 # PUT ALL THE CLASSY CODE HERE
 if __name__ =="__main__":
